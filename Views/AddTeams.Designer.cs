33e8bd74d70e5b3d91c39928acdcb5d6f2a9014d
Sharfaraz <rownak.shr@gmail.com>
2023-09-05 21:25:55 +0600

    Create Team UI

---
+++ Views/AddTeams.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace EventController.Views
+{
+    partial class AddTeams
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TeamImgLbl = new System.Windows.Forms.Label();
+            this.TeamImageBox = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PassTxtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.Label = new System.Windows.Forms.Label();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TeamImgLbl
+            // 
+            this.TeamImgLbl.AutoSize = true;
+            this.TeamImgLbl.Location = new System.Drawing.Point(348, 144);
+            this.TeamImgLbl.Name = "TeamImgLbl";
+            this.TeamImgLbl.Size = new System.Drawing.Size(61, 13);
+            this.TeamImgLbl.TabIndex = 20;
+            this.TeamImgLbl.Text = "Team Logo";
+            // 
+            // TeamImageBox
+            // 
+            this.TeamImageBox.Location = new System.Drawing.Point(351, 118);
+            this.TeamImageBox.Name = "TeamImageBox";
+            this.TeamImageBox.Size = new System.Drawing.Size(142, 23);
+            this.TeamImageBox.TabIndex = 18;
+            this.TeamImageBox.Text = "Select Image";
+            this.TeamImageBox.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label2.Location = new System.Drawing.Point(22, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 25);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Team Name";
+            // 
+            // PassTxtBox
+            // 
+            this.PassTxtBox.Location = new System.Drawing.Point(27, 121);
+            this.PassTxtBox.Name = "PassTxtBox";
+            this.PassTxtBox.Size = new System.Drawing.Size(263, 20);
+            this.PassTxtBox.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label1.Location = new System.Drawing.Point(22, 352);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 25);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Owner Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 252);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(263, 20);
+            this.textBox1.TabIndex = 25;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label3.Location = new System.Drawing.Point(22, 491);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 25);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Manager Name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(27, 380);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(263, 20);
+            this.textBox2.TabIndex = 27;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label5.Location = new System.Drawing.Point(22, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(240, 25);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Team Name Short Form";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(27, 519);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(263, 20);
+            this.textBox3.TabIndex = 29;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label6.Location = new System.Drawing.Point(346, 93);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 25);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Team Logo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label7.Location = new System.Drawing.Point(346, 224);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(141, 25);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "Team Banner";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label8.Location = new System.Drawing.Point(346, 352);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(138, 25);
+            this.label8.TabIndex = 33;
+            this.label8.Text = "Owner Image";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label9.Location = new System.Drawing.Point(346, 491);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(161, 25);
+            this.label9.TabIndex = 34;
+            this.label9.Text = "Manager Image";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(351, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 23);
+            this.button1.TabIndex = 35;
+            this.button1.Text = "Select Image";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(351, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 23);
+            this.button2.TabIndex = 36;
+            this.button2.Text = "Select Image";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(351, 516);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 23);
+            this.button3.TabIndex = 37;
+            this.button3.Text = "Select Image";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(348, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "Team Banner";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(348, 403);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 13);
+            this.label10.TabIndex = 39;
+            this.label10.Text = "Owner Image";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(348, 542);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(81, 13);
+            this.label11.TabIndex = 40;
+            this.label11.Text = "Manager Image";
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.Label.Location = new System.Drawing.Point(193, 9);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(246, 29);
+            this.Label.TabIndex = 41;
+            this.Label.Text = "INSERT TEAM INFO";
+            this.Label.Click += new System.EventHandler(this.Label_Click);
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.LoginButton.Location = new System.Drawing.Point(524, 608);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(103, 45);
+            this.LoginButton.TabIndex = 42;
+            this.LoginButton.Text = "Insert";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.button4.Location = new System.Drawing.Point(404, 608);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 45);
+            this.button4.TabIndex = 43;
+            this.button4.Text = "Cancel";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // AddTeams
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 675);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PassTxtBox);
+            this.Controls.Add(this.TeamImgLbl);
+            this.Controls.Add(this.TeamImageBox);
+            this.Name = "AddTeams";
+            this.Text = "AddTeams";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TeamImgLbl;
+        private System.Windows.Forms.Button TeamImageBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PassTxtBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ Views/AddTeams.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventController.Views
+{
+    public partial class AddTeams : Form
+    {
+        public AddTeams()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
